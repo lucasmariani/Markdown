3dd430e94d3adc66c7065f0f06cbf8cf1eada958
Lucas Mariani <lmariani@icloud.com>
2026-03-04 18:13:23 -0300

    Initial Commit

---
+++ Markdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Markdown
+//
+//  Created by Lucas on 4/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Markdown/MarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownApp.swift
+//  Markdown
+//
+//  Created by Lucas on 4/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
